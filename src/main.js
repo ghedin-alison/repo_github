25e425d62f40b4353e598a84ce58246bb0409f56
Alison Ghedin <alison.ghedin@outlook.com>
2020-09-27 09:50:48 -0300

    configurando o repositorio

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
-document.querySelector('body').style.background= '#7E625D';
+//document.querySelector('body').style.background= '#7E625D';
+class App{
+    constructor(){
+        // lista de repositorios
+        this.repositorios = [];
+
+        // Recuperar o formulario
+        this.formulario = document.querySelector('form');
+
+        // Método para registrar os eventos do formulário
+        this.registrarEventos();
+    }
+    
+    registrarEventos(){
+        this.formulario.onsubmit = evento => this.adicionarRepositorio(evento)
+    }
+    
+    adicionarRepositorio(evento){
+        // Evita que o formulário recarregue a pagina
+        evento.preventDefault();
+        // Adiciona o repositorio na lista
+        this.repositorios.push({
+            nome: 'Nerd Fonts',
+            descricao: 'Iconic font aggregator, collection and patcher',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/8083459?v=4',
+            link: 'https://github.com/ryanoasis/nerd-fonts'
+        });
+
+        // Renderizar a tela
+        console.log(this.repositorios);
+    }
+};
+
+new App();
